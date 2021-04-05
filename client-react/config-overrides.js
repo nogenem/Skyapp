8982f8ebf32acd7420c3b98cf3db39a88fb202d2
Gilney N. Mathias <gilney_salvo@hotmail.com>
2021-04-05 15:01:09 -0300

    p01: Fix a STUPID problem
    I really hate everyone on create-react-app' team...

--- client-react/config-overrides.js
+++ client-react/config-overrides.js
@@ -1,6 +1,10 @@
 const { alias, configPaths } = require('react-app-rewire-alias');
 
 const { useBabelRc } = require('customize-cra');
+const os = require('os');
+
+// PS: I really hate everyone on create-react-app' team...
+Object.defineProperty(os, 'EOL', { value: '\n' });
 
 // https://github.com/oklas/react-app-rewire-alias/issues/1
 module.exports = function override(config) {
