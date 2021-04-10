a9daa865d8cb2a4f50cb4e964fd468b8c70e829e
Gilney N. Mathias <gilney_salvo@hotmail.com>
2021-04-10 10:19:54 -0300

    p01: Use `UserStatusDot` in `ChatAvatar`

--- client-react/src/components/ChatAvatar/ChatAvatar.tsx
+++ client-react/src/components/ChatAvatar/ChatAvatar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
-import { Avatar as MuiAvatar, Badge } from '@material-ui/core';
+import { Avatar as MuiAvatar } from '@material-ui/core';
 import { AccountCircle as AccountCircleIcon } from '@material-ui/icons';
 
-import { USER_STATUS, USER_STATUS_2_TEXT } from '~/constants/user_status';
 import type { TUserStatus } from '~/constants/user_status';
 
+import { UserStatusDot } from '../UserStatusDot';
 import useStyles from './useStyles';
 
 interface IOwnProps {
@@ -17,59 +16,18 @@ interface IOwnProps {
 type TProps = IOwnProps;
 
 const ChatAvatar = ({ online, status }: TProps) => {
-  const { t: trans } = useTranslation(['Common']);
   const classes = useStyles();
 
-  const title = getDotTitle(online, status);
-
-  // TODO: Use `UserStatusDot`
   return (
-    <Badge
-      overlap="circle"
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-      variant="dot"
-      color="secondary"
-      title={trans(`Common:${title}`)}
-      classes={{
-        root: classes.badge,
-        dot: classes.dotBadge,
-        colorSecondary: getDotStyle(classes, online, status),
-        anchorOriginBottomRightCircle: classes.anchorOriginBottomRightCircle,
-      }}
-    >
+    <div style={{ position: 'relative' }}>
       <MuiAvatar>
-        <AccountCircleIcon />
+        <AccountCircleIcon className={classes.icon} />
       </MuiAvatar>
-    </Badge>
+      <span className={classes.dotContainer}>
+        <UserStatusDot online={online} status={status} />
+      </span>
+    </div>
   );
-};
-
-const getDotTitle = (online: boolean, status: TUserStatus): string => {
-  if (!online) return 'Offline';
-  return USER_STATUS_2_TEXT[status];
-};
-
-const getDotStyle = (
-  classes: ReturnType<typeof useStyles>,
-  online: boolean,
-  status: TUserStatus,
-): string => {
-  if (!online) return classes.dotBadge_offline;
-  switch (status) {
-    case USER_STATUS.ACTIVE:
-      return classes.dotBadge_active;
-    case USER_STATUS.AWAY:
-      return classes.dotBadge_away;
-    case USER_STATUS.DO_NOT_DISTURB:
-      return classes.dotBadge_do_not_disturb;
-    case USER_STATUS.INVISIBLE:
-      return classes.dotBadge_invisible;
-    default:
-      return '';
-  }
 };
 
 export type { TProps };
--- client-react/src/components/ChatAvatar/useStyles.ts
+++ client-react/src/components/ChatAvatar/useStyles.ts
@@ -2,63 +2,18 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    badge: {
-      alignItems: 'center',
+    container: {
+      position: 'relative',
     },
-    dotBadge: {
-      height: '14px',
-      minWidth: '14px',
-      border: `2px solid ${theme.palette.background.default}`,
-      borderRadius: '50%',
+    icon: {
+      width: '100%',
+      height: '100%',
     },
-    dotBadge_active: {
-      backgroundColor: '#44b700',
-    },
-    dotBadge_away: {
-      backgroundColor: '#FFBA00',
-
-      '&::before': {
-        content: "''",
-        position: 'absolute',
-
-        backgroundColor: 'white',
-        width: '1px',
-        height: '5px',
-        top: '5px',
-        left: '5px',
-        transform: 'translate(-50%, -88%)',
-      },
-      '&::after': {
-        content: "''",
-        position: 'absolute',
-
-        backgroundColor: 'white',
-        width: '1px',
-        height: '4px',
-        top: '5px',
-        left: '5px',
-        transform: 'translate(100%, -16%) rotate(120deg)',
-      },
-    },
-    dotBadge_do_not_disturb: {
-      backgroundColor: '#f44336',
-
-      '&::after': {
-        content: "''",
-        position: 'absolute',
-        backgroundColor: 'white',
-        width: '6px',
-        height: '2px',
-      },
-    },
-    dotBadge_invisible: {
-      backgroundColor: 'white',
-    },
-    dotBadge_offline: {
-      backgroundColor: '#f44336',
-    },
-    anchorOriginBottomRightCircle: {
-      bottom: '18%',
+    dotContainer: {
+      position: 'absolute',
+      bottom: '0',
+      right: '0',
+      display: 'flex',
     },
   }),
 );
