f152c4cf34b0fe3d1017a5859670e3841c4c7dba
Gilney N Mathias <gilney_salvo@hotmail.com>
2021-11-13 09:36:30 -0300

    p02: Emit messages only to other users
    Had to do this cause of the queue on the front-end

--- server-node/src/services/IoEmitters/messagesReceived.ts
+++ server-node/src/services/IoEmitters/messagesReceived.ts
@@ -5,10 +5,11 @@ import type { TEmitterFunc, IMessagesReceived } from './types';
 const messagesReceived: TEmitterFunc = async (io, clients, eventData) => {
   const event = IO_MESSAGES_RECEIVED;
   const { channel, messages } = eventData as IMessagesReceived;
+  const fromId = messages[0].from_id;
 
   channel.members.forEach(member => {
     const thisMemberClient = clients[member.user_id];
-    if (thisMemberClient) {
+    if (thisMemberClient && (!fromId || fromId !== member.user_id)) {
       io.to(thisMemberClient.socketId).emit(event, messages);
     }
   });
