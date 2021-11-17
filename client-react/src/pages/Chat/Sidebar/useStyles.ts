cc36ad9f064b58c71954d0745684db0f7ed97b2e
Gilney N Mathias <gilney_salvo@hotmail.com>
2021-11-17 19:37:51 -0300

    p01: Temporarily fix a problem

--- client-react/src/pages/Chat/Sidebar/useStyles.ts
+++ client-react/src/pages/Chat/Sidebar/useStyles.ts
@@ -3,9 +3,10 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      minWidth: '300px',
+      width: '330px',
       height: '100vh',
       padding: theme.spacing(1),
+      // TODO: Think of a way to make this really responsive...
       // [theme.breakpoints.down(850)]: {
       //   width: '100%',
       // },
