d463fab67a1d006dfce8d20219e9c096cdb6cf3a
Gilney N Mathias <gilney_salvo@hotmail.com>
2021-12-01 09:09:32 -0300

    p01: Update the `redux/chat` tests

--- client-react/src/redux/chat/__tests__/actions.test.ts
+++ client-react/src/redux/chat/__tests__/actions.test.ts
@@ -1,9 +1,43 @@
+import { Socket } from 'socket.io-client';
+
+import { MESSAGE_TYPES } from '~/constants/message_types';
 import * as SOCKET_EVENTS from '~/constants/socket_events';
+import { USER_STATUS } from '~/constants/user_status';
 import { TUserState } from '~/redux/user/types';
+import ApiService from '~/services/ApiService';
 import IoService from '~/services/IoService';
+import MessageQueueService, {
+  QUEUE_ACTIONS,
+} from '~/services/MessageQueueService';
 import { setupFakeSocket, getMockStore, FACTORIES } from '~/utils/testUtils';
 
-import { userSignedIn, userSignedOut } from '../actions';
+import {
+  emitSetActiveChannel,
+  emitSetLastSeen,
+  enqueueSendDeleteMessage,
+  enqueueSendEditTextMessage,
+  enqueueSendFileMessages,
+  enqueueSendTextMessage,
+  sendCreateChannelWith,
+  sendCreateGroupChannel,
+  sendGetMessages,
+  sendLeaveGroupChannel,
+  sendUpdateGroupChannel,
+  userSignedIn,
+  userSignedOut,
+} from '../actions';
+import {
+  EChatActions,
+  IDeleteMessageCredentials,
+  IEditMessageCredentials,
+  IFetchMessagesCredentials,
+  ILeaveGroupCredentials,
+  IMessage,
+  INewGroupCredentials,
+  IOtherUser,
+  ISendMessageCredentials,
+  IUpdateGroupCredentials,
+} from '../types';
 
 jest.mock('socket.io-client');
 
@@ -12,7 +46,15 @@ const mockStore = getMockStore();
 describe('chat actions', () => {
   const fakeSocket = setupFakeSocket();
 
+  let ioServer: IoService;
+
+  beforeEach(() => {
+    ioServer = IoService.instance(true);
+  });
+
   afterEach(() => {
+    ioServer.disconnect();
+
     jest.restoreAllMocks();
   });
 
@@ -26,14 +68,289 @@ describe('chat actions', () => {
     // Since fakeSocket does not automatically emit a connect message as socketio io() does, simulate it here.
     fakeSocket.server!.emit(SOCKET_EVENTS.SOCKET_CONNECT);
 
-    const instance = IoService.instance();
-    expect(instance!.socket!.connected).toBe(true);
+    expect(ioServer!.socket!.connected).toBe(true);
   });
 
   it('userSignedOut', () => {
+    ioServer.connect();
+
     userSignedOut()();
 
-    const instance = IoService.instance();
-    expect(instance!.socket!.connected).toBe(false);
+    expect(ioServer!.socket!.connected).toBe(false);
+  });
+
+  it('sendCreateChannelWith', async () => {
+    const channelId = '1';
+    const user: IOtherUser = {
+      _id: '1',
+      nickname: 'Test 1',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: true,
+    };
+    const apiResponse = { channel_id: channelId };
+
+    const expectedActions = [
+      {
+        type: EChatActions.ACTIVE_CHANNEL_CHANGED,
+        payload: { _id: apiResponse.channel_id },
+      },
+    ];
+    const spy = jest
+      .spyOn(ApiService.chat, 'createChannelWith')
+      .mockImplementationOnce(() => {
+        return Promise.resolve(apiResponse);
+      });
+    const store = mockStore({});
+
+    await sendCreateChannelWith(user)(store.dispatch);
+
+    expect(spy).toHaveBeenCalledWith(user);
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('sendCreateGroupChannel', async () => {
+    const channelId = '1';
+    const credentials: INewGroupCredentials = {
+      name: 'Group 1',
+      members: ['1', '2'],
+      admins: ['1'],
+    };
+    const apiResponse = { channel_id: channelId };
+
+    const expectedActions = [
+      {
+        type: EChatActions.ACTIVE_CHANNEL_CHANGED,
+        payload: { _id: apiResponse.channel_id },
+      },
+    ];
+    const spy = jest
+      .spyOn(ApiService.chat, 'createGroupChannel')
+      .mockImplementationOnce(() => {
+        return Promise.resolve(apiResponse);
+      });
+    const store = mockStore({});
+
+    await sendCreateGroupChannel(credentials)(store.dispatch);
+
+    expect(spy).toHaveBeenCalledWith(credentials);
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('sendUpdateGroupChannel', async () => {
+    const channelId = '1';
+    const credentials: IUpdateGroupCredentials = {
+      channel_id: channelId,
+      name: 'Group 1',
+      members: ['1', '2'],
+      admins: ['1'],
+    };
+    const apiResponse = { channel_id: channelId };
+
+    const expectedActions = [
+      {
+        type: EChatActions.ACTIVE_CHANNEL_CHANGED,
+        payload: { _id: apiResponse.channel_id },
+      },
+    ];
+    const spy = jest
+      .spyOn(ApiService.chat, 'updateGroupChannel')
+      .mockImplementationOnce(() => {
+        return Promise.resolve(apiResponse);
+      });
+    const store = mockStore({});
+
+    await sendUpdateGroupChannel(credentials)(store.dispatch);
+
+    expect(spy).toHaveBeenCalledWith(credentials);
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('sendLeaveGroupChannel', async () => {
+    const channelId = '1';
+    const credentials: ILeaveGroupCredentials = {
+      channel_id: channelId,
+    };
+    const apiResponse = { channel_id: channelId };
+
+    const expectedActions = [
+      {
+        type: EChatActions.REMOVED_FROM_CHANNEL,
+        payload: { channelId: apiResponse.channel_id },
+      },
+    ];
+    const spy = jest
+      .spyOn(ApiService.chat, 'leaveGroupChannel')
+      .mockImplementationOnce(() => {
+        return Promise.resolve(apiResponse);
+      });
+    const store = mockStore({});
+
+    await sendLeaveGroupChannel(credentials)(store.dispatch);
+
+    expect(spy).toHaveBeenCalledWith(credentials);
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('sendGetMessages', async () => {
+    const credentials: IFetchMessagesCredentials = {
+      channel_id: '1',
+      offset: 0,
+      limit: 10,
+      sort: '-createdAt',
+    };
+    const apiResponse = { docs: [], totalDocs: 0 };
+
+    const expectedActions = [
+      {
+        type: EChatActions.NEW_MESSAGES_RECEIVED,
+        payload: {
+          messages: apiResponse.docs,
+          totalMessages: apiResponse.totalDocs,
+          atTop: true,
+        },
+      },
+    ];
+    const spy = jest
+      .spyOn(ApiService.chat, 'getMessages')
+      .mockImplementationOnce(() => {
+        return Promise.resolve(apiResponse);
+      });
+    const store = mockStore({});
+
+    await sendGetMessages(credentials)(store.dispatch);
+
+    expect(spy).toHaveBeenCalledWith(credentials);
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('enqueueSendTextMessage', async () => {
+    const channelId = '1';
+    const message = 'Test Message';
+
+    const expectedCredentials: ISendMessageCredentials = {
+      channel_id: channelId,
+      body: message,
+    };
+
+    const spy = jest
+      .spyOn(MessageQueueService, 'enqueue')
+      .mockImplementationOnce(() => {});
+
+    await enqueueSendTextMessage(channelId, message)();
+
+    expect(spy).toHaveBeenCalledWith(
+      expectedCredentials,
+      QUEUE_ACTIONS.SEND_TEXT_MESSAGE,
+    );
+  });
+
+  it('enqueueSendFileMessages', async () => {
+    const filesData = new FormData();
+
+    const spy = jest
+      .spyOn(MessageQueueService, 'enqueue')
+      .mockImplementationOnce(() => {});
+
+    await enqueueSendFileMessages(filesData)();
+
+    expect(spy).toHaveBeenCalledWith(
+      filesData,
+      QUEUE_ACTIONS.SEND_FILE_MESSAGES,
+    );
+  });
+
+  it('enqueueSendEditTextMessage', async () => {
+    const message: IMessage = {
+      _id: '1',
+      channel_id: '1',
+      body: 'Test Message',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      type: MESSAGE_TYPES.TEXT,
+    };
+    const newBody = 'New Test Message';
+
+    const expectedCredentials: IEditMessageCredentials = {
+      message,
+      newBody,
+    };
+    const spy = jest
+      .spyOn(MessageQueueService, 'enqueue')
+      .mockImplementationOnce(() => {});
+
+    await enqueueSendEditTextMessage(message, newBody)();
+
+    expect(spy).toHaveBeenCalledWith(
+      expectedCredentials,
+      QUEUE_ACTIONS.EDIT_TEXT_MESSAGE,
+    );
+  });
+
+  it('enqueueSendDeleteMessage', async () => {
+    const message: IMessage = {
+      _id: '1',
+      channel_id: '1',
+      body: 'Test Message',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      type: MESSAGE_TYPES.TEXT,
+    };
+
+    const expectedCredentials: IDeleteMessageCredentials = {
+      message,
+    };
+    const spy = jest
+      .spyOn(MessageQueueService, 'enqueue')
+      .mockImplementationOnce(() => {});
+
+    await enqueueSendDeleteMessage(message)();
+
+    expect(spy).toHaveBeenCalledWith(
+      expectedCredentials,
+      QUEUE_ACTIONS.DELETE_MESSAGE,
+    );
+  });
+
+  it('emitSetActiveChannel', () => {
+    const channelId = '1';
+
+    ioServer.connect();
+    const socket = ioServer.socket as Socket;
+
+    const expectedActions = [
+      {
+        type: EChatActions.ACTIVE_CHANNEL_CHANGED,
+        payload: { _id: channelId },
+      },
+    ];
+    const spy = jest.spyOn(socket, 'emit').mockImplementationOnce(() => {
+      return socket;
+    });
+    const store = mockStore({});
+
+    emitSetActiveChannel(channelId)(store.dispatch);
+
+    expect(spy).toHaveBeenCalledWith(SOCKET_EVENTS.IO_SET_ACTIVE_CHANNEL, {
+      channel_id: channelId,
+    });
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('emitSetLastSeen', () => {
+    const channelId = '1';
+
+    ioServer.connect();
+    const socket = ioServer.socket as Socket;
+
+    const spy = jest.spyOn(socket, 'emit').mockImplementationOnce(() => {
+      return socket;
+    });
+
+    emitSetLastSeen(channelId)();
+
+    expect(spy).toHaveBeenCalledWith(SOCKET_EVENTS.IO_SET_LAST_SEEN, {
+      channel_id: channelId,
+    });
   });
 });
--- client-react/src/redux/chat/__tests__/reducer.test.ts
+++ client-react/src/redux/chat/__tests__/reducer.test.ts
@@ -1,10 +1,43 @@
+import { MESSAGE_TYPES } from '~/constants/message_types';
 import { USER_STATUS } from '~/constants/user_status';
+import { EUserActions, TUserAction } from '~/redux/user/types';
 
-import userReducer from '../reducer';
-import type { TChatAction, IInitialData, TChatState } from '../types';
+import userReducer, { initialState as reducerInitialState } from '../reducer';
+import type {
+  TChatAction,
+  IInitialData,
+  TChatState,
+  IChannel,
+  IOtherUser,
+  IMessage,
+} from '../types';
 import { EChatActions } from '../types';
 
 describe('chat reducer', () => {
+  it('should handle SIGNED_OUT', () => {
+    const userId = '1';
+    const initialState: TChatState = {
+      users: {
+        [userId]: {
+          _id: userId,
+          nickname: 'Test',
+          thoughts: '',
+          status: USER_STATUS.ACTIVE,
+          online: false,
+        },
+      },
+      channels: {},
+    };
+    const action: TUserAction = {
+      type: EUserActions.SIGNED_OUT,
+      payload: null,
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.users).toEqual(reducerInitialState.users);
+    expect(newState.channels).toEqual(reducerInitialState.channels);
+  });
+
   it('should handle INITIAL_DATA_LOADED', () => {
     const initialData: IInitialData = { users: {}, channels: {} };
     const action: TChatAction = {
@@ -18,7 +51,7 @@ describe('chat reducer', () => {
   });
 
   it('should handle USER_ONLINE_STATUS_CHANGED', () => {
-    const userId = '123456';
+    const userId = '1';
     const initialState: TChatState = {
       users: {
         [userId]: {
@@ -50,5 +83,641 @@ describe('chat reducer', () => {
 
     newState = userReducer(initialState, action);
     expect(newState.users[userId].online).toEqual(action.payload.value);
+  });
+
+  it('should handle USER_ONLINE_STATUS_CHANGED', () => {
+    const user1 = {
+      _id: '1',
+      nickname: 'Test 1',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: true,
+    };
+    const user2 = {
+      _id: '2',
+      nickname: 'Test 2',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: true,
+    };
+    const initialState: TChatState = {
+      users: {
+        [user1._id]: user1,
+        [user2._id]: user2,
+      },
+      channels: {},
+    };
+
+    // new channel
+    let channelId = 'abcdef';
+    const payload1: IChannel = {
+      _id: channelId,
+      name: 'Channel 1',
+      is_group: false,
+      members: [
+        {
+          user_id: user1._id,
+          is_adm: false,
+          last_seen: new Date(),
+        },
+        {
+          user_id: user2._id,
+          is_adm: false,
+          last_seen: new Date(),
+        },
+      ],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    let action: TChatAction = {
+      type: EChatActions.CHANNEL_CREATED_OR_UPDATED,
+      payload: payload1,
+    };
+
+    let newState = userReducer(initialState, action);
+    expect(newState.channels[channelId]).toBeTruthy();
+    expect(newState.channels[channelId].name).toEqual(user2.nickname);
+    expect(newState.users[user1._id].channel_id).toEqual(channelId);
+    expect(newState.users[user2._id].channel_id).toEqual(channelId);
+
+    // update channel
+    const payload2: IChannel = {
+      ...payload1,
+      other_member_idx: 0,
+    };
+    action = {
+      type: EChatActions.CHANNEL_CREATED_OR_UPDATED,
+      payload: payload2,
+    };
+
+    newState = userReducer(initialState, action);
+    expect(newState.channels[channelId]).toBeTruthy();
+    expect(newState.channels[channelId].name).toEqual(user1.nickname);
+    expect(newState.users[user1._id].channel_id).toEqual(channelId);
+    expect(newState.users[user2._id].channel_id).toEqual(channelId);
+  });
+
+  it('should handle ACTIVE_CHANNEL_CHANGED', () => {
+    const channelId = '1';
+    const channel: IChannel = {
+      _id: channelId,
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 10,
+    };
+
+    // setting the activeChannel
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channelId]: channel,
+      },
+    };
+    let action: TChatAction = {
+      type: EChatActions.ACTIVE_CHANNEL_CHANGED,
+      payload: { _id: channelId },
+    };
+
+    let newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo).toBeTruthy();
+    expect(newState.activeChannelInfo?._id).toBe(channelId);
+    expect(newState.channels[channelId].unread_msgs).toBe(0);
+
+    // Resetting the activeChannel
+    action = {
+      type: EChatActions.ACTIVE_CHANNEL_CHANGED,
+      payload: { _id: undefined },
+    };
+
+    newState = userReducer(newState, action);
+    expect(newState.activeChannelInfo).toBeFalsy();
+  });
+
+  it('should handle REMOVED_FROM_CHANNEL', () => {
+    const channelId = '1';
+    const channel: IChannel = {
+      _id: channelId,
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 10,
+    };
+
+    // setting the activeChannel
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channelId]: channel,
+      },
+      activeChannelInfo: {
+        _id: channelId,
+        messages: [],
+        totalMessages: 0,
+        queue: [],
+      },
+    };
+    const action: TChatAction = {
+      type: EChatActions.REMOVED_FROM_CHANNEL,
+      payload: { channelId },
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.channels[channelId]).toBeFalsy();
+    expect(newState.activeChannelInfo).toBeFalsy();
+  });
+
+  it('should handle NEW_USER_CONFIRMED', () => {
+    const userId = '1';
+    const otherUser: IOtherUser = {
+      _id: userId,
+      nickname: 'Test',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: false,
+    };
+    const initialState: TChatState = {
+      users: {},
+      channels: {},
+    };
+
+    const action: TChatAction = {
+      type: EChatActions.NEW_USER_CONFIRMED,
+      payload: otherUser,
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.users[userId]).toBeTruthy();
+  });
+
+  it('should handle NEW_MESSAGES_RECEIVED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const message1: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const message2: IMessage = {
+      _id: '3',
+      body: 'Test message 2',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const messages: IMessage[] = [message1];
+
+    // With activeChannelInfo
+    let initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [message2],
+        totalMessages: 1,
+        queue: [],
+      },
+    };
+
+    const action: TChatAction = {
+      type: EChatActions.NEW_MESSAGES_RECEIVED,
+      payload: {
+        messages,
+        totalMessages: -1,
+        atTop: true,
+      },
+    };
+
+    let newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.messages[0]).toEqual(message1);
+    expect(newState.activeChannelInfo?.totalMessages).toEqual(2);
+    expect(newState.channels[channel._id].lastMessage).toEqual(message1);
+
+    // Without activeChannelInfo
+    initialState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+    };
+
+    newState = userReducer(initialState, action);
+    expect(newState.channels[channel._id].unread_msgs).toEqual(1);
+    expect(newState.channels[channel._id].lastMessage).toEqual(message1);
+  });
+
+  it('should handle MESSAGE_ENQUEUED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const message: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [],
+        totalMessages: 0,
+        queue: [],
+      },
+    };
+
+    const action: TChatAction = {
+      type: EChatActions.MESSAGE_ENQUEUED,
+      payload: message,
+    };
+
+    let newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.queue[0]).toEqual(message);
+  });
+
+  it('should handle MESSAGE_DEQUEUED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const message: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [],
+        totalMessages: 0,
+        queue: [message],
+      },
+    };
+
+    const action: TChatAction = {
+      type: EChatActions.MESSAGE_DEQUEUED,
+      payload: message,
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.queue.length).toBe(0);
+  });
+
+  it('should handle LAST_SEEN_CHANGED', () => {
+    const user = {
+      _id: '1',
+      nickname: 'Test 1',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: true,
+    };
+    const channel: IChannel = {
+      _id: '2',
+      name: 'Channel 1',
+      is_group: false,
+      members: [
+        {
+          user_id: user._id,
+          is_adm: false,
+          last_seen: new Date(),
+        },
+      ],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+
+    const initialState: TChatState = {
+      users: {
+        [user._id]: user,
+      },
+      channels: {
+        [channel._id]: channel,
+      },
+    };
+
+    const newLastSeen = '2021-11-29T14:04:06.091Z';
+    const action: TChatAction = {
+      type: EChatActions.LAST_SEEN_CHANGED,
+      payload: {
+        channel_id: channel._id,
+        user_id: user._id,
+        last_seen: '2021-11-29T14:04:06.091Z',
+      },
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(
+      newState.channels[channel._id].members[0].last_seen.toISOString(),
+    ).toBe(newLastSeen);
+  });
+
+  it('should handle USER_STATUS_CHANGED', () => {
+    const user = {
+      _id: '1',
+      nickname: 'Test 1',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: true,
+    };
+
+    const initialState: TChatState = {
+      users: {
+        [user._id]: user,
+      },
+      channels: {},
+    };
+
+    const newStatus = USER_STATUS.AWAY;
+    const action: TChatAction = {
+      type: EChatActions.USER_STATUS_CHANGED,
+      payload: {
+        user_id: user._id,
+        newStatus,
+      },
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.users[user._id].status).toBe(newStatus);
+  });
+
+  it('should handle USER_THOUGHTS_CHANGED', () => {
+    const user = {
+      _id: '1',
+      nickname: 'Test 1',
+      thoughts: '',
+      status: USER_STATUS.ACTIVE,
+      online: true,
+    };
+
+    const initialState: TChatState = {
+      users: {
+        [user._id]: user,
+      },
+      channels: {},
+    };
+
+    const newThoughts = 'New thoughts...';
+    const action: TChatAction = {
+      type: EChatActions.USER_THOUGHTS_CHANGED,
+      payload: {
+        user_id: user._id,
+        newThoughts,
+      },
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.users[user._id].thoughts).toBe(newThoughts);
+  });
+
+  it('should handle MESSAGE_IS_UPDATING_CHANGED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const message: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isUpdating: false,
+    };
+
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [message],
+        totalMessages: 1,
+        queue: [],
+      },
+    };
+
+    // value = true
+    let action: TChatAction = {
+      type: EChatActions.MESSAGE_IS_UPDATING_CHANGED,
+      payload: {
+        message_id: message._id,
+        value: true,
+      },
+    };
+
+    let newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.messages[0].isUpdating).toBe(true);
+
+    // value = false
+    action = {
+      type: EChatActions.MESSAGE_IS_UPDATING_CHANGED,
+      payload: {
+        message_id: message._id,
+        value: false,
+      },
+    };
+
+    newState = userReducer(newState, action);
+    expect(newState.activeChannelInfo?.messages[0].isUpdating).toBe(false);
+  });
+
+  it('should handle MESSAGE_UPDATED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const initialMessage: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isUpdating: false,
+    };
+    channel.lastMessage = initialMessage;
+
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [initialMessage],
+        totalMessages: 1,
+        queue: [],
+      },
+    };
+
+    const newMessage = { ...initialMessage, body: 'Updated text...' };
+    const action: TChatAction = {
+      type: EChatActions.MESSAGE_UPDATED,
+      payload: newMessage,
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.messages[0].body).toBe(newMessage.body);
+    expect(newState.channels[channel._id].lastMessage?.body).toBe(
+      newMessage.body,
+    );
+  });
+
+  it('should handle MESSAGE_IS_DELETING_CHANGED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const message: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isDeleting: false,
+    };
+
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [message],
+        totalMessages: 1,
+        queue: [],
+      },
+    };
+
+    // value = true
+    let action: TChatAction = {
+      type: EChatActions.MESSAGE_IS_DELETING_CHANGED,
+      payload: {
+        message_id: message._id,
+        value: true,
+      },
+    };
+
+    let newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.messages[0].isDeleting).toBe(true);
+
+    // value = false
+    action = {
+      type: EChatActions.MESSAGE_IS_DELETING_CHANGED,
+      payload: {
+        message_id: message._id,
+        value: false,
+      },
+    };
+
+    newState = userReducer(newState, action);
+    expect(newState.activeChannelInfo?.messages[0].isDeleting).toBe(false);
+  });
+
+  it('should handle MESSAGE_DELETED', () => {
+    const channel: IChannel = {
+      _id: '1',
+      name: 'Channel 1',
+      is_group: false,
+      members: [],
+      other_member_idx: 1,
+      unread_msgs: 0,
+    };
+    const message1: IMessage = {
+      _id: '2',
+      body: 'Test message 1',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const message2: IMessage = {
+      _id: '3',
+      body: 'Test message 2',
+      channel_id: channel._id,
+      type: MESSAGE_TYPES.TEXT,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    channel.lastMessage = message2;
+
+    const initialState: TChatState = {
+      users: {},
+      channels: {
+        [channel._id]: channel,
+      },
+      activeChannelInfo: {
+        _id: channel._id,
+        messages: [message1, message2],
+        totalMessages: 2,
+        queue: [],
+      },
+    };
+
+    const action: TChatAction = {
+      type: EChatActions.MESSAGE_DELETED,
+      payload: {
+        message: message2,
+        lastMessage: message1,
+      },
+    };
+
+    const newState = userReducer(initialState, action);
+    expect(newState.activeChannelInfo?.messages.length).toBe(1);
+    expect(newState.activeChannelInfo?.messages[0]).toBe(message1);
+    expect(newState.channels[channel._id].lastMessage).toBe(message1);
   });
 });
