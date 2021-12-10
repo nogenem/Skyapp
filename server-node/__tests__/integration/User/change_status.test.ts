2cc4c031e000a0454a0f7cf830568fbb1fb1d2f6
Gilney N Mathias <gilney_salvo@hotmail.com>
2021-12-10 09:27:29 -0300

    p02: Update the `User` tests

--- server-node/__tests__/integration/User/change_status.test.ts
+++ server-node/__tests__/integration/User/change_status.test.ts
@@ -2,10 +2,12 @@ import jsonwebtoken from 'jsonwebtoken';
 import supertest from 'supertest';
 
 import app from '~/app';
+import { IO_USER_STATUS_CHANGED } from '~/constants/socket_events';
 import { USER_STATUS } from '~/constants/user_status';
 import type { IChangeStatusCredentials } from '~/controllers';
 import { User } from '~/models';
 import type { IUserDoc } from '~/models';
+import { IoService } from '~/services';
 import factory from '~t/factories';
 import { setupDB } from '~t/test-setup';
 
@@ -24,14 +26,19 @@ describe('Change_Status', () => {
     const user: IUserDoc = await factory.create<IUserDoc>('User', {
       status: USER_STATUS.ACTIVE,
     });
-    const credentials: IChangeStatusCredentials = {
-      newStatus: USER_STATUS.AWAY,
-    };
+    const newStatus = USER_STATUS.AWAY;
 
     jest.spyOn(jsonwebtoken, 'verify').mockImplementation(token => {
       if (token === VALID_TOKEN) return { _id: user._id };
       throw new Error();
     });
+
+    const io = IoService.instance();
+    const ioSpy = jest.spyOn(io, 'emit').mockReturnValueOnce(Promise.resolve());
+
+    const credentials: IChangeStatusCredentials = {
+      newStatus,
+    };
 
     const res = await request
       .post('/api/user/change_status')
@@ -43,21 +50,29 @@ describe('Change_Status', () => {
     const userRecord = (await User.findOne({
       _id: user._id,
     })) as IUserDoc;
-    expect(userRecord.status).toBe(USER_STATUS.AWAY);
+    expect(userRecord.status).toBe(newStatus);
+
+    expect(ioSpy).toHaveBeenCalled();
+    expect(ioSpy.mock.calls[0][0]).toBe(IO_USER_STATUS_CHANGED);
+    expect(ioSpy.mock.calls[0][1]).toEqual({
+      user_id: user._id,
+      newStatus,
+    });
   });
 
   it('should return 304 (Not Modified) if trying to change to the same status', async () => {
     const user: IUserDoc = await factory.create<IUserDoc>('User', {
       status: USER_STATUS.AWAY,
     });
-    const credentials: IChangeStatusCredentials = {
-      newStatus: USER_STATUS.AWAY,
-    };
 
     jest.spyOn(jsonwebtoken, 'verify').mockImplementation(token => {
       if (token === VALID_TOKEN) return { _id: user._id };
       throw new Error();
     });
+
+    const credentials: IChangeStatusCredentials = {
+      newStatus: USER_STATUS.AWAY,
+    };
 
     const res = await request
       .post('/api/user/change_status')
--- server-node/__tests__/integration/User/change_thoughts.test.ts
+++ server-node/__tests__/integration/User/change_thoughts.test.ts
@@ -2,9 +2,11 @@ import jsonwebtoken from 'jsonwebtoken';
 import supertest from 'supertest';
 
 import app from '~/app';
+import { IO_USER_THOUGHTS_CHANGED } from '~/constants/socket_events';
 import type { IChangeThoughtsCredentials } from '~/controllers';
 import { User } from '~/models';
 import type { IUserDoc } from '~/models';
+import { IoService } from '~/services';
 import factory from '~t/factories';
 import { setupDB } from '~t/test-setup';
 
@@ -23,14 +25,19 @@ describe('Change_Thoughts', () => {
     const user: IUserDoc = await factory.create<IUserDoc>('User', {
       thoughts: 'Some thoughts...',
     });
-    const credentials: IChangeThoughtsCredentials = {
-      newThoughts: 'hello world',
-    };
+    const newThoughts = 'hello world';
 
     jest.spyOn(jsonwebtoken, 'verify').mockImplementation(token => {
       if (token === VALID_TOKEN) return { _id: user._id };
       throw new Error();
     });
+
+    const io = IoService.instance();
+    const ioSpy = jest.spyOn(io, 'emit').mockReturnValueOnce(Promise.resolve());
+
+    const credentials: IChangeThoughtsCredentials = {
+      newThoughts,
+    };
 
     const res = await request
       .post('/api/user/change_thoughts')
@@ -42,21 +49,29 @@ describe('Change_Thoughts', () => {
     const userRecord = (await User.findOne({
       _id: user._id,
     })) as IUserDoc;
-    expect(userRecord.thoughts).toBe(credentials.newThoughts);
+    expect(userRecord.thoughts).toBe(newThoughts);
+
+    expect(ioSpy).toHaveBeenCalled();
+    expect(ioSpy.mock.calls[0][0]).toBe(IO_USER_THOUGHTS_CHANGED);
+    expect(ioSpy.mock.calls[0][1]).toEqual({
+      user_id: user._id,
+      newThoughts,
+    });
   });
 
   it('should return 304 (Not Modified) if trying to change to the same thoughts', async () => {
     const user: IUserDoc = await factory.create<IUserDoc>('User', {
       thoughts: 'hello world',
     });
-    const credentials: IChangeThoughtsCredentials = {
-      newThoughts: 'hello world',
-    };
 
     jest.spyOn(jsonwebtoken, 'verify').mockImplementation(token => {
       if (token === VALID_TOKEN) return { _id: user._id };
       throw new Error();
     });
+
+    const credentials: IChangeThoughtsCredentials = {
+      newThoughts: 'hello world',
+    };
 
     const res = await request
       .post('/api/user/change_thoughts')
