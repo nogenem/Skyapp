60d0a88929e6326c7d7cb604ed028bb278489f89
Gilney N Mathias <gilney_salvo@hotmail.com>
2022-01-08 13:56:53 -0300

    p03: Initial commit

---
+++ server-spring/src/main/java/com/nogenem/skyapp/SkyappApplication.java
@@ -0,0 +1,13 @@
+package com.nogenem.skyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyappApplication.class, args);
+	}
+
+}
